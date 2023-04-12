98ac28309cc8035ed7e916c9c7abd3dc7ad65ac4
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-04-12 09:32:11 +0530

    Added: Employee Payroll Client Server

---
+++ js/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    use_local_storage: "false",
+    home_page: "../pages/HomePage.html",
+    add_employee_page:"../pages/Form_Employee_Payroll.html",
+    server_url: "http://localhost:3000/EmployeePayrollDB/"
+}; 
